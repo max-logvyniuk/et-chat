7b90ada420314bf332d53c7375776037137bb76e
Ваше ім'я <maroon4m@gmail.com>
2020-04-24 13:09:37 +0300

    update CheckToken

--- api/server/src/utils/CheckToken.js
+++ api/server/src/utils/CheckToken.js
@@ -8,10 +8,11 @@ const currentToken = {
 
 export default async function CheckToken(request, response, next) {
   const headerAuth = request.headers.authorization;
-  console.info('CCCCCCCCCCCC!!!!!', request.query, typeof request.body.UserId );
+  console.info('Request.query!!!!!', request.query);
   console.info('Token and current', headerAuth, currentToken );
+
   if (process.env.USE_TOKEN_CHEKING !== 'off') {
-    if (request.query.user !== '200' || !request.query.user ) {
+    if (request.query.user !== '200' || !request.query.user) {
       if (headerAuth&& isEmpty(currentToken) || headerAuth !== currentToken.current) {
         const token = _split(headerAuth, ' ')[1];
         const payload = await fetch("https://security-system-auth.herokuapp.com/api/verifying/user" ,{
@@ -22,7 +23,7 @@ export default async function CheckToken(request, response, next) {
           }
         });
         console.info('Auth response', payload);
-        if (payload.status === "error") {
+        if (payload.status === 401) {
           console.info('NOT VERIFIED');
           return response.status(401).json({message: 'NOT VERIFIED'})
         }
